0567093db8be3da54f68e36a26f5cdf416f09fb4
Edward Ndukui <edwardquixote>
2020-03-09 12:16:29 +0300

    Initial commit.

---
+++ app/src/main/java/uk/co/edwardquixote/Zalego/AMFirstProject/FirstActivity.java
@@ -0,0 +1,14 @@
+package uk.co.edwardquixote.Zalego.AMFirstProject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
