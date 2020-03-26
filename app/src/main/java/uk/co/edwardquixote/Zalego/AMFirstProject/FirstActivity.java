09a00408feceb5b72735c3277d6ca5b349795223
Edward Ndukui <edwardquixote>
2020-03-26 19:49:13 +0300

    Implemented Activity Lifecycle methods in FirstActivity.

--- app/src/main/java/uk/co/edwardquixote/Zalego/AMFirstProject/FirstActivity.java
+++ app/src/main/java/uk/co/edwardquixote/Zalego/AMFirstProject/FirstActivity.java
@@ -11,4 +11,46 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
 }
